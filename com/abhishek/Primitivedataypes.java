04a9244ff5b3e27df86845bebdcbacf723b5acc9
AbhishekGirpunje <151643397+AbhishekGirpunje@users.noreply.github.com>
2025-01-21 11:13:58 +0000

    sum, primitive, typecasting

---
+++ com/abhishek/Primitivedataypes.java
@@ -0,0 +1,12 @@
+
+public class Primitivedatatypes {
+    public static void main(String[] args){
+        int rollNo = 69;
+        char letter = 'r';
+        float marks = 98.69f;
+        double largeDecimalNumber = 1234568765.345;
+        long LargeIntValue = 1234567898765432L;
+        boolean check = false;
+
+    }
+}
